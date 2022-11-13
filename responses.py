560dcd9e1e6992ddbac29b24e3541f6711c189d4
Geoff Hodgson <geoffphodgson1@gmail.com>
2022-11-13 17:45:51 -0600

    cleanup

--- responses.py
+++ responses.py
@@ -16,19 +16,3 @@ def handle_response(msg) -> str:
             view = InvaildRequestView([])
 
         return view.createOutput()
-        
-        if(p_msg.startswith('team ')):
-            p_msg = p_msg.lstrip('team ')
-            team_id = p_msg
-            teamService = teams.teamService.TeamService(requestService)
-            team = teamService.getTeamFromId(team_id)
-            view = teams.views.teamView.TeamView(team)
-
-            return view.createOutput()
-        
-        if(p_msg.startswith('teams')):
-            teamService = teams.teamService.TeamService(requestService)
-            teamsList = teamService.getTeams()
-            view = teams.views.teamsView.TeamsView(teamsList)
-
-            return view.createOutput()
